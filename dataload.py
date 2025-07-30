fe7aa4c005c111f27e65ed9a80f9b87097a9f1da
SeongYoon <zeroplam5@gmail.com>
2025-07-30 18:58:15 +0900

    dataload changed

--- dataload.py
+++ dataload.py
@@ -7,6 +7,9 @@ def load_task1_data(path="task1_data.jsonl"):
     data = []
     with open(path, "r", encoding="utf-8") as f:
         for line in f:
+            line = line.strip()
+            if not line:  # 빈 줄이면 건너뜀
+                continue
             obj = json.loads(line)
             data.append(obj)
     return data
